ff75cba533bc5032b550b8294eeb04b3ab5e803a
Andre Soesilo <asoesilo84@gmail.com>
2014-12-04 22:40:17 -0800

    Add a new model to represent the teams in NBA

---
+++ app/models/nba.rb
@@ -0,0 +1,36 @@
+class NBA
+  cattr_reader :teams
+
+  @@teams = {
+    "Dallas Mavericks" => "dallas-mavericks",
+    "New York Knicks" => "new-york-knicks",
+    "Miami Heat" => "miami-heat",
+    "Los Angeles Lakers" => "los-angeles-lakers",
+    "Chicago Bulls" => "chicago-bulls",
+    "Brooklyn Nets" => "brooklyn-nets",
+    "Boston Celtics" => "boston-celtics",
+    "Los Angeles Clippers" => "los-angeles-clippers",
+    "Golden State Warriors" => "golden-state-warriors",
+    "San Antonio Spurs" => "san-antonio-spurs",
+    "Washington Wizards" => "washington-wizards",
+    "Oklahoma City Thunder" => "oklahoma-city-thunder",
+    "Philadelphia 76ers" => "philadelphia-76ers",
+    "Orlando Magic" => "orlando-magic",
+    "Indiana Pacers" => "indiana-pacers",
+    "Portland Trail Blazers" => "portland-trail-blazers",
+    "Atlanta Hawks" => "atlanta-hawks",
+    "Memphis Grizzlies" => "memphis-grizzlies",
+    "Denver Nuggets" => "denver-nuggets",
+    "Toronto Raptors" => "toronto-raptors",
+    "Houston Rockets" => "houston-rockets",
+    "Charlotte Hornets" => "charlotte-hornets",
+    "Phoenix Suns" => "phoenix-suns",
+    "Minnesota Timberwolves" => "minnesota-timberwolves",
+    "New Orleans Pelicans" => "new-orleans-pelicans",
+    "Sacramento Kings" => "sacramento-kings",
+    "Detroit Pistons" => "detroit-pistons",
+    "Milwaukee Bucks" => "milwaukee-bucks",
+    "Utah Jazz" => "utah-jazz",
+    "Cleveland Cavaliers" => "cleveland-cavaliers"
+  }
+end
