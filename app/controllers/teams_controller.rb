8f8cfddeb38b87bad2eb05cc5b3afd53260ab9a7
Andre Soesilo <asoesilo84@gmail.com>
2014-12-06 22:59:17 -0800

    Add a new function to return the list of team events in the team controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,4 +2,11 @@ class TeamsController < ApplicationController
   def index
     render json: NBA.teams.values
   end
+
+  def events
+    id = params[:team_id].to_i
+    slug = NBA.teams[id].slug
+    events = Event.fetch_events(slug: slug)
+    render json: events
+  end
 end
